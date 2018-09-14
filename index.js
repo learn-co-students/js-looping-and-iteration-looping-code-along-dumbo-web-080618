cc08c9b904cad9f3bcb426d89aa7a61e048a325f
github username <github email address>
2018-09-14 18:46:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function printBadges(a){
+  for (let i = 0; i < a.length; i++){
+    console.log(`Welcome ${a[i]}! You are employee #${i + 1}.`);
+  }
+  return a
+}
+
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
